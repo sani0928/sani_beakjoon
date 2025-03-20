0a5f0a984cdb5e13d119f862c058cb24de0ce8d5
jehyun <jeahyunning@naver.com>
2025-03-20 17:52:04 +0900

    03201751

--- n&m/15651.py
+++ n&m/15651.py
@@ -1,17 +1,18 @@
-import sys;sys.stdin=open('15650.txt')
+import sys;sys.stdin=open('15651.txt')
 
 N, M = map(int,input().split())
-
+result = []
 def back(idx,lst):
 
     if len(lst) == M:
         print(*lst)
         return
 
-
     for i in range(idx,N+1):
         lst.append(i)
-        back(i+1,lst)
+        back(idx+1,lst)
         lst.pop()
 
 back(1,[])
+
+
